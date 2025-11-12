3cf21c2a452b33b03924a4cfa0594eff7f94e24b
Golden_Reflection <reflectiongolden@gmail.com>
2025-11-12 13:49:50 +0000

    Changed title and app component to reflect the purpose of the project

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Essentials Site</h1>
+        <p>This project is here to demonstrate the use of ReactJS for the creation of a website.</p>
       </header>
     </div>
   );
