addcdf9fc31a03a01d0bc7b4b76fb824c72b04fa
Golden_Reflection <reflectiongolden@gmail.com>
2025-11-24 11:20:37 +0000

    Created ModuleTwoPage component in preparation for making a page for each module to decrease need for scrolling

--- src/App.js
+++ src/App.js
@@ -9,27 +9,33 @@ function App() {
         <h1>React Essentials Site</h1>
         <p>This project is here to demonstrate the use of ReactJS for the creation of a website.</p>
       </header>
-      <div>
-        <h2>Profile Card component</h2>
-        <p>These "Profile Cards" make use of a reusable component, showcasing componenets and their ability to be reused for display purposes. They make use of Props to be able to send through images, names, job titles, and a bio for the person being referenced. Due to these being examples, I have chosen to use silhouette heads for the images and Lorem Ipsum text for the bios.</p>
-        <ProfileCard 
-          img={man} 
-          name="John Smith"
-          jobTitle="Manager"
-          bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc enim orci, fermentum at malesuada at, tincidunt ac neque. Vestibulum vulputate eget tellus ac sollicitudin. Donec quis magna non mauris viverra luctus aliquet ac metus. Fusce consequat vestibulum mi eu dictum. Aenean id metus a ipsum luctus commodo ut quis ipsum."
-          skills={["Communication", "Problem-solving", "Leadership"]}
-        />
-        <ProfileCard 
+      <ModuleTwoPage/>
+    </div>
+  );
+}
+
+function ModuleTwoPage() {
+  return (
+    <div>
+      <h2>Profile Card component</h2>
+      <p>These "Profile Cards" make use of a reusable component, showcasing componenets and their ability to be reused for display purposes. They make use of Props to be ableto send through images, names, job titles, and a bio for the person being referenced. Due to these being examples, I have chosen to use silhouette heads for the imagesand Lorem Ipsum text for the bios.</p>
+      <ProfileCard 
+        img={man} 
+        name="John Smith"
+        jobTitle="Manager"
+        bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc enim orci, fermentum at malesuada at, tincidunt ac neque. Vestibulum vulputate eget tellus ac sollicitudin. Donec quis magna non mauris viverra luctus aliquet ac metus. Fusce consequat vestibulum mi eu dictum. Aenean id metus a ipsum luctus commodo ut quis ipsum."
+        skills={["Communication", "Problem-solving", "Leadership"]}
+      />
+      <ProfileCard 
         img={woman}
         name="Jane Doe"
         jobTitle="Head of HR"
-        bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id enim sed lectus sagittis tincidunt id eget risus. Curabitur id nibh rutrum, laoreet neque eu, sollicitudin eros. Nam in nunc egestas, porta metus non, mollis diam. Donec accumsan tellus purus, et ultrices est tristique ut."
+        bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id enim sed lectus sagittis tincidunt id eget risus. Curabitur id nibh rutrum, laoreet neque eu sollicitudin eros. Nam in nunc egestas, porta metus non, mollis diam. Donec accumsan tellus purus, et ultrices est tristique ut."
         skills={["Test skill", "Example 2", "Another example"]}
-        />
-        <VariableDisplay/>
-      </div>
+      />
+      <VariableDisplay/>
     </div>
-  );
+  )
 }
 
 function ProfileCard(props) {
