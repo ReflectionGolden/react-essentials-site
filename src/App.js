6d244c050160b88cabb8d4e24a63f446c0d041b9
Golden_Reflection <reflectiongolden@gmail.com>
2025-11-24 11:54:03 +0000

    Added component container for a better view of which component is being viewed, and added JobBoard component

--- src/App.js
+++ src/App.js
@@ -17,23 +17,32 @@ function App() {
 function ModuleTwoPage() {
   return (
     <div>
-      <h2>Profile Card component</h2>
-      <p>These "Profile Cards" make use of a reusable component, showcasing componenets and their ability to be reused for display purposes. They make use of Props to be ableto send through images, names, job titles, and a bio for the person being referenced. Due to these being examples, I have chosen to use silhouette heads for the imagesand Lorem Ipsum text for the bios.</p>
-      <ProfileCard 
-        img={man} 
-        name="John Smith"
-        jobTitle="Manager"
-        bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc enim orci, fermentum at malesuada at, tincidunt ac neque. Vestibulum vulputate eget tellus ac sollicitudin. Donec quis magna non mauris viverra luctus aliquet ac metus. Fusce consequat vestibulum mi eu dictum. Aenean id metus a ipsum luctus commodo ut quis ipsum."
-        skills={["Communication", "Problem-solving", "Leadership"]}
-      />
-      <ProfileCard 
-        img={woman}
-        name="Jane Doe"
-        jobTitle="Head of HR"
-        bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id enim sed lectus sagittis tincidunt id eget risus. Curabitur id nibh rutrum, laoreet neque eu sollicitudin eros. Nam in nunc egestas, porta metus non, mollis diam. Donec accumsan tellus purus, et ultrices est tristique ut."
-        skills={["Test skill", "Example 2", "Another example"]}
-      />
-      <VariableDisplay/>
+      <h1>Module Two Components</h1>
+      <p>The components you see here were created in accordance with Module 2 of the React Essentials course by ITOL.</p>
+      <div className='Component-Container'>
+        <h2>Profile Card component</h2>
+        <p>These "Profile Cards" make use of a reusable component, showcasing componenets and their ability to be reused for display purposes. They make use of Props to be ableto send through images, names, job titles, and a bio for the person being referenced. Due to these being examples, I have chosen to use silhouette heads for the imagesand Lorem Ipsum text for the bios.</p>
+        <ProfileCard 
+          img={man} 
+          name="John Smith"
+          jobTitle="Manager"
+          bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc enim orci, fermentum at malesuada at, tincidunt ac neque. Vestibulum vulputate eget tellus ac sollicitudin. Donec quis magna non mauris viverra luctus aliquet ac metus. Fusce consequat vestibulum mi eu dictum. Aenean id metus a ipsum luctus commodo ut quis ipsum."
+          skills={["Communication", "Problem-solving", "Leadership"]}
+        />
+        <ProfileCard 
+          img={woman}
+          name="Jane Doe"
+          jobTitle="Head of HR"
+          bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id enim sed lectus sagittis tincidunt id eget risus. Curabitur id nibh rutrum, laoreet neque eu sollicitudin eros. Nam in nunc egestas, porta metus non, mollis diam. Donec accumsan tellus purus, et ultrices est tristique ut."
+          skills={["Test skill", "Example 2", "Another example"]}
+        />
+      </div>
+      <div className='Component-Container'>
+        <VariableDisplay/>
+      </div>
+      <div className='Component-Container'>
+        <JobBoard/>
+      </div>
     </div>
   )
 }
@@ -81,6 +90,42 @@ function VariableDisplay() {
       <p>Object Name: {objVar.name}</p>
       <p>Object Age: {objVar.age}</p>
       <p>Object Hair Colour: {objVar.hairColour}</p>
+    </div>
+  )
+}
+
+function JobBoard() {
+  let jobCount = Math.floor(Math.random() * 10);
+  let companyName = "Lorem Ipsum Ltd.";
+
+  const getJobMessage = () => {
+    if (jobCount === 0) {
+      return("No Jobs for scheduling today.");
+    }
+    else if (jobCount > 0 && jobCount <= 5) {
+      return(`Number of jobs in the Job Board: ${jobCount}`);
+    }
+    else {
+      return(`There are ${jobCount} jobs in the Job Board, better get scheduling!`);
+    }
+  }
+
+  const getExpectedJobs = () => {
+    if (jobCount === 0) {
+      return("No jobs expected next week.");
+    }
+    else {
+      return(`Expected number of Jobs next week: ${Math.ceil(jobCount * 1.5)}`);
+    }
+  }
+
+  return (
+    <div>
+      <h2>Job Board</h2>
+      <p>This component is set up to showcase the use of JSX expressions and conditional rendering. The number of jobs is randomised upon component loading, in order to demonstrate different conditions.</p>
+      <h2>{companyName}</h2>
+      <p>{getJobMessage()}</p>
+      <p>{getExpectedJobs()}</p>
     </div>
   )
 }
