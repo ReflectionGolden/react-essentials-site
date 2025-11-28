e28b03669a1903b09c7a5420f4f54d8ae9752d95
Golden_Reflection <reflectiongolden@gmail.com>
2025-11-28 11:07:44 +0000

    Updated JobCounter to include decrementing and resetting the count, as well as tracking multiple environments with a way to toggle between them while saving the values

--- src/course_modules/Module3.js
+++ src/course_modules/Module3.js
@@ -50,16 +50,64 @@ function InlineStyle() {
 }
 
 function JobCounter() {
-    const [jobCount, setJobCount] = useState(0);
-    function AddJob() {
-        setJobCount(jobCount+1);
+    const [isProduction, setEnv] = useState(true);
+    function ToggleEnv() {
+        setEnv(!isProduction);
     }
     return (
         <div>
             <h2>Job Counter Component</h2>
-            <p>Current Job Count: {jobCount}</p>
-            <button className='Generic-button' onClick={AddJob}>Add Job</button>
-            <p>This component shows the use of states and events to increment a number upon a button-click. This functionality is also demonstrated by the different module pages and the buttons to swap between them, seen in the navigation at the top of the page.</p>
+            <p>Current Environment: {isProduction ? "Production" : "UAT"}</p>
+            <button className='Generic-button' onClick={ToggleEnv}>Toggle Environment</button>
+            <ProdCounter isHidden={isProduction}/>
+            <UATCounter isHidden={!isProduction}/>
+            <p>
+                This component shows the use of states and events to increment, decrement, or reset a number upon a button-click, including making use of useState so that the rendered counter updates to show the new value. This functionality is also demonstrated by the different module pages and the buttons to swap between them, seen in the navigation at the top of the page.
+                <br />
+                I have also implemented toggling between two separate "environments" which will save their individual job counts when toggled, so Production can have jobs added (until it is, for example, at 8 jobs) then you can switch to UAT and add more jobs (for example, up to 23) and when you toggle back to Production it will show the 8 jobs that were added to Production (and toggling to UAT again will show its count of 23).
+            </p>
+        </div>
+    );
+}
+
+function ProdCounter({isHidden}) {
+    const [jobCountProd, setJobCountProd] = useState(0);
+    function AddJobProd() {
+        setJobCountProd(jobCountProd+1);
+    }
+    function RemoveJobProd() {
+        setJobCountProd(jobCountProd-1);
+    }
+    function ResetJobsProd() {
+        setJobCountProd(0);
+    }
+    return (
+        <div hidden={isHidden}>
+            <p>Current Job Count: {jobCountProd}</p>
+            <button className='Generic-button' onClick={AddJobProd}>Add Job</button>
+            <button className='Generic-button' onClick={RemoveJobProd}>Remove Job</button>
+            <button className='Generic-button' onClick={ResetJobsProd}>Reset Job Count</button>
+        </div>
+    );
+}
+
+function UATCounter({isHidden}) {
+    const [jobCountUAT, setJobCountUAT] = useState(0);
+    function AddJobUAT() {
+        setJobCountUAT(jobCountUAT+1);
+    }
+    function RemoveJobUAT() {
+        setJobCountUAT(jobCountUAT-1);
+    }
+    function ResetJobsUAT() {
+        setJobCountUAT(0);
+    }
+    return (
+        <div hidden={isHidden}>
+            <p>Current Job Count: {jobCountUAT}</p>
+            <button className='Generic-button' onClick={AddJobUAT}>Add Job</button>
+            <button className='Generic-button' onClick={RemoveJobUAT}>Remove Job</button>
+            <button className='Generic-button' onClick={ResetJobsUAT}>Reset Job Count</button>
         </div>
     );
 }
