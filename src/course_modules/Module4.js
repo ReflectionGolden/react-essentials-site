8d9b0711cb099e50222219e15c46437dfa55dc88
Golden_Reflection <reflectiongolden@gmail.com>
2025-12-05 13:36:03 +0000

    Initial creation of dynamic bot manager component

--- src/course_modules/Module4.js
+++ src/course_modules/Module4.js
@@ -5,8 +5,37 @@ function ModuleFourPage() {
     return (
         <div>
             <h1>Module Four Components</h1>
+            <div className='Component-Container'>
+                <DynamicBotManager/>
+            </div>
         </div>
     );
+}
+
+function DynamicBotManager() {
+    const [bots, setBots] = useState([
+        {id: "01", name: "EmailBot", status: "Active"},
+        {id: "02", name: "DataBot", status: "Inactive"}
+    ]);
+    return (
+        <div>
+            <h2>Dynamic Bot Manager Component</h2>
+            <div className='Bot-Inputs'>
+                <h3>Bot Creation</h3>
+                {/* Bot input fields */}
+            </div>
+            <div className='Bots-Display'>
+                <h3>Current Bot list</h3>
+                <ul className='Listing'>
+                    {bots.map(bot => (
+                        <li key={bot.id}>{bot.id}-{bot.name}, Status: {bot.status}</li>
+                    ))}
+                </ul>
+                
+                {/* Bot display case */}
+            </div>
+        </div>
+    )
 }
 
 export default ModuleFourPage;
