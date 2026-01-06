4c2bdb32136cdd480ab68a42945379f28e122114
Golden_Reflection <reflectiongolden@gmail.com>
2026-01-06 13:28:29 +0000

    changed Job Type inputs to use a reusable component, and added FontAwesome icons for easy visualisation of job status

--- src/course_modules/Module5.js
+++ src/course_modules/Module5.js
@@ -1,5 +1,14 @@
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../App.css';
+import './module_styles/Module5.css';
+
+//Font Awesome Icons
+import { faClipboardList } from '@fortawesome/free-solid-svg-icons';
+import { faPersonRunning } from '@fortawesome/free-solid-svg-icons';
+import { faCircleCheck } from '@fortawesome/free-regular-svg-icons';
+import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
+
 
 function ModuleFivePage() {
     return (
@@ -90,47 +99,64 @@ function JobForm() {
             <h2>Job Form Component</h2>
             <p>
                 This component demonstrates the use of multiple input types through the use of a job creation form including a number input with a built in minimum, a text input, a radio button selection, and a dropdown selection. It also includes validation for fields to be required, and clears the inputs when a job is successfully created.
+                <br />
+                I have also styled the form using a separate css file to make it visually better than just having the different inputs stacked on top of each other, and also increased input sizes for better readability.
             </p>
-            <form onSubmit={e => handleSubmission(e)}>
-                <label htmlFor="jobID">Job ID: </label>
-                <input type="number" min="0" id='jobID' value={idVal} placeholder='Enter ID' onChange={handleIdChange}/><br /><br />
+            <form className='Form-Container' onSubmit={e => handleSubmission(e)}>
+                <div className='idInput'>
+                    <label htmlFor="jobID">Job ID: </label>
+                    <input type="number" min="0" id='jobID' value={idVal} placeholder='Enter ID' onChange={handleIdChange}/><br /><br />
+                </div>
 
-                <label htmlFor="jobTitle">Job Title: </label>
-                <input type="text" id='jobTitle' value={nameVal} placeholder='Enter Job Title' onChange={handleNameChange}/><br /><br />
+                <div className='titleInput'>
+                    <label htmlFor="jobTitle">Job Title: </label>
+                    <input type="text" id='jobTitle' value={nameVal} placeholder='Enter Job Title' onChange={handleNameChange}/><br /><br />
+                </div>
 
-                <label htmlFor="jobType">Job Type: </label><br />
-                <input type="radio" name="jobType" id="readEmails" checked={typeVal === "readEmailsJob"} value={"readEmailsJob"} onChange={handleTypeChange}/>
-                <label htmlFor="readEmails">Read Emails</label><br />
-                <input type="radio" name="jobType" id="sendEmails" checked={typeVal === "sendEmailsJob"} value={"sendEmailsJob"} onChange={handleTypeChange}/>
-                <label htmlFor="sendEmails">Send Emails</label><br />
-                <input type="radio" name="jobType" id="webParse" checked={typeVal === "webParseJob"} value={"webParseJob"} onChange={handleTypeChange}/>
-                <label htmlFor="webParse">Web Parsing</label><br /><br />
+                <div className='typeInput'>
+                    <div>
+                        <label htmlFor="jobType">Job Type: </label><br />
+                        <div>
+                            <TypeRadio typeLabel="Read Emails" typeID="readEmails" typeVal={typeVal} value="readEmailsJob" handleTypeChange={handleTypeChange}/><br />
+                            <TypeRadio typeLabel="Send Emails" typeID="sendEmails" typeVal={typeVal} value="sendEmailsJob" handleTypeChange={handleTypeChange}/><br />
+                            <TypeRadio typeLabel="Web Parsing" typeID="webParse" typeVal={typeVal} value="webParseJob" handleTypeChange={handleTypeChange}/><br />
+                        </div>
+                    </div>
+                </div>
                 
-                <label htmlFor="jobStatus">Job Status: </label><br />
-                <select name="jobStatus" id="jobStatus" value={statusVal} onChange={handleStatusChange}>
-                    <option value="" disabled>Select a Status</option>
-                    <option value="pending">Start Pending</option>
-                    <option value="running">Running</option>
-                    <option value="completed">Completed</option>
-                    <option value="stopped">Stopped</option>
-                </select><br />
+                <div className='statusInput'>
+                    <div>
+                        <label htmlFor="jobStatus">Job Status: </label><br />
+                        <select name="jobStatus" id="jobStatus" value={statusVal} onChange={handleStatusChange}>
+                            <option value="" disabled>Select a Status</option>
+                            <option value="pending">Start Pending</option>
+                            <option value="running">Running</option>
+                            <option value="completed">Completed</option>
+                            <option value="stopped">Stopped</option>
+                        </select><br />
+                    </div>
+                </div>
 
-                <p>
-                    <span style={{textDecoration: "underline"}}>Job Preview:</span> <br />
-                    ID: {idVal} <br />
-                    Name: {nameVal} <br />
-                    Type: {typeVal} <br />
-                    Status: {statusVal}
-                </p>
+                <div className='jobPreview'>
+                    <p>
+                        <span style={{textDecoration: "underline"}}>Job Preview:</span> <br />
+                        ID: {idVal} <br />
+                        Name: {nameVal} <br />
+                        Type: {typeVal} <br />
+                        Status: {statusVal}
+                    </p>
+                </div>
 
-                <button className='Generic-button' id="submit">Submit</button>
+                <div className='jobSubmit'>
+                    <button className='Generic-button' id="submit">Submit</button>
+                </div>
             </form>
             <div>
                 <h2>Current Jobs List</h2>
                 <ul className='Listing'>
                     {jobs.map(job => (
                         <JobDisplayItem job={job}>
-                            ID: {job.id}, Name: {job.name}, Job Type: {job.type}, Status: {job.status}
+                            <p>ID: {job.id}, Name: {job.name}, Job Type: {job.type}, Status: {job.status}<StatusIcon Status={job.status}/></p>
                             <button style={{marginLeft: "5px"}} onClick={() => handleDelete(job.id)}>Delete</button>
                         </JobDisplayItem>
                     ))}
@@ -145,6 +171,38 @@ function JobDisplayItem(props) {
         <li key={props.job.id}>
             {props.children}
         </li>
+    );
+}
+
+function TypeRadio({typeLabel, typeId, typeVal, value, handleTypeChange}) {
+    return (
+        <>
+            <input type="radio" name="jobType" id={typeId} checked={typeVal === value} value={value} onChange={handleTypeChange}/>
+            <label htmlFor={typeId}>{typeLabel}</label>
+        </>
+    );
+}
+
+function StatusIcon({Status}) {
+    function StatIcon(Status) {
+        switch (Status) {
+            case "pending":
+                return(<FontAwesomeIcon icon={faClipboardList} style={{color: "#FFD43B",}} />);
+            case "running":
+                return(<FontAwesomeIcon icon={faPersonRunning} style={{color: "#74C0FC",}} />);
+            case "completed":
+                return(<FontAwesomeIcon icon={faCircleCheck} style={{color: "#00ff00",}} />);
+            case "stopped":
+                return(<FontAwesomeIcon icon={faCircleXmark} style={{color: "#ff0000",}} />);
+            default:
+                break;
+        }
+    }
+
+    return (
+        <>
+            {StatIcon(Status)}
+        </>
     );
 }
 
