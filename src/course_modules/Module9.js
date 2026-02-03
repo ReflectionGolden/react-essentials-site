7e53d9b795155a85ccc80375a1b3b38a666acea5
Golden_Reflection <reflectiongolden@gmail.com>
2026-02-03 10:11:48 +0000

    Added Movie Review links to Module 9 page

--- src/course_modules/Module9.js
+++ src/course_modules/Module9.js
@@ -10,7 +10,7 @@ function ModuleNinePage() {
             <div className='Component-Container'>
                 <h2>Password Generator</h2>
                 <p>
-                    The Movie Review App has been created as a separate project at my <a className='App-link' href="" target='_blank'>movie-review-project</a><a className='App-link' href="" target="_blank"><FontAwesomeIcon icon={faGithub}/></a> based on the lessons and practical activities provided within module 9 of the ITOL React Essentials course.
+                    The Movie Review App has been created as a separate project at my <a className='App-link' href="https://reflectiongolden.github.io/movie-review-project/" target='_blank'>movie-review-project</a><a className='App-link' href="https://github.com/ReflectionGolden/movie-review-project" target="_blank"><FontAwesomeIcon icon={faGithub}/></a> based on the lessons and practical activities provided within module 9 of the ITOL React Essentials course.
                 </p>
             </div>
         </div>
