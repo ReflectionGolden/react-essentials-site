fcc1cc8f59a0497a69059deafc8eaa23eb86197b
Golden_Reflection <63142898+ReflectionGolden@users.noreply.github.com>
2026-02-16 10:52:25 +0000

    Fixed incorrect title

--- src/course_modules/Module9.js
+++ src/course_modules/Module9.js
@@ -8,7 +8,7 @@ function ModuleNinePage() {
         <div>
             <h1>Module 9&10: Movie Review App</h1>
             <div className='Component-Container'>
-                <h2>Password Generator</h2>
+                <h2>Movie Review App</h2>
                 <p>
                     The Movie Review App has been created as a separate project at my <a className='App-link' href="https://reflectiongolden.github.io/movie-review-project/" target='_blank'>movie-review-project</a><a className='App-link' href="https://github.com/ReflectionGolden/movie-review-project" target="_blank"><FontAwesomeIcon icon={faGithub}/></a> based on the lessons and practical activities provided within module 9 and module 10 of the ITOL React Essentials course.
                 </p>
